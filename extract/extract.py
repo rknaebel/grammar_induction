ec1cc1922f2049c6b8dbff16ba5353a0e9736859
James Longbotham <james.longbotham@gmail.com>
2015-06-03 23:46:31 +0200

    Added first extract script, corpus, alignments

---
+++ extract/extract.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""
+Reads in the geoquery corpus and writes three text files:
+1. Strings
+2. Syntax trees
+3. Variable free semantic representation (functional query language)
+
+These can then be used as parallel corpora
+"""
+
+from bs4 import BeautifulSoup
+
+def list_to_txt(xml_list):
+    txt = ""
+    for item in xml_list:
+        txt = txt + item + "\n"
+    # have to remove last newline for Giza++
+    return txt[:(len(txt)-2)]
+
+
+def main():
+
+    soup = BeautifulSoup(open("../data/corpus.xml"), "xml")
+    
+    # extract strings
+    string = []
+    raw_string = soup("nl")  
+    for item in raw_string:
+        if item["lang"] == "en":
+            string.append(item.string.replace("\n",""))
+    
+    string_txt = open("../data/string.txt", "w")
+    # changes casing to all lower case for word alignment
+    string_txt.write(list_to_txt(string).lower())
+    string_txt.close()
+    
+    # extract syntax
+    syntax = []
+    raw_syntax = soup("syn")
+    for item in raw_syntax:
+        if item["lang"] == "en":
+            syntax.append(item.string.replace("\n",""))
+       
+    syntax_txt = open("../data/syntax.txt", "w")
+    syntax_txt.write(list_to_txt(syntax))
+    syntax_txt.close()
+    
+    # extract variable-free gequery
+    geo_funql = []
+    raw_geo_funql = soup("mrl")
+    for item in raw_geo_funql:
+        if item["lang"] == "geo-funql":
+            geo_funql.append(item.string.replace("\n",""))
+    
+    geo_funql_txt = open("../data/geo-funql.txt", "w")
+    geo_funql_txt.write(list_to_txt(geo_funql))
+    geo_funql_txt.close()
+
+
+if __name__ == "__main__":
+    main()
+
