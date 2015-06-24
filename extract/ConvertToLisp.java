53627f6177cdda42ca03eafee186ca6eb8b091d9
Rene Knaebel <reneknaebel90@gmail.com>
2015-06-24 22:39:55 +0200

    added java converter to lisp format 1st version

---
+++ extract/ConvertToLisp.java
@@ -0,0 +1,55 @@
+/**
+* autor:
+*
+* date:
+*
+**/
+
+// package
+
+
+// import
+import de.up.ling.irtg.corpus.Corpus;
+import de.up.ling.irtg.InterpretedTreeAutomaton;
+import de.up.ling.irtg.algebra.Algebra;
+import de.up.ling.irtg.algebra.StringAlgebra;
+import de.up.ling.irtg.algebra.TreeAlgebra;
+import de.up.ling.irtg.corpus.Instance;
+
+import de.up.ling.tree.Tree;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+import java.io.Reader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// class definition
+public class ConvertToLisp {
+
+    public static void main(String[] args) {
+        
+        try {
+            FileInputStream grammarStream = new FileInputStream(args[0]);
+            FileInputStream corpusStream  = new FileInputStream(args[1]);
+            
+            InterpretedTreeAutomaton irtg = InterpretedTreeAutomaton.read(grammarStream);
+            Reader corpusReader           = new InputStreamReader(corpusStream);
+            Corpus corpus = irtg.readCorpus(corpusReader);
+
+            for (Instance ins : corpus) {
+                Map<String,Object> tMap = ins.getInputObjects();
+                Tree t = (Tree)tMap.get("t");
+                System.out.println(t.toLispString());
+            }
+        
+        } catch(Exception e) {
+            //System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+}
