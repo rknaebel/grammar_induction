ca631f4a623f87b888ce03c57c129f7f83c9b0af
Rene Knaebel <reneknaebel90@gmail.com>
2015-07-15 17:10:19 +0200

    fixed the current version for now, but will check the changes in alto to get rid of the errors

--- run.sh
+++ run.sh
@@ -1,28 +1,37 @@
 
 ALIGNMENT=data/string2geo.A3.final5BEST
+GDIR=generated
 
-python induct/induct.py $ALIGNMENT left  nosplit   > generated/grammar1.irtg
-python induct/induct.py $ALIGNMENT right nosplit   > generated/grammar2.irtg
-python induct/induct.py $ALIGNMENT both  nosplit   > generated/grammar3.irtg
-python induct/induct.py $ALIGNMENT left  semsplit  > generated/grammar4.irtg
-python induct/induct.py $ALIGNMENT right semsplit  > generated/grammar5.irtg
-python induct/induct.py $ALIGNMENT both  semsplit  > generated/grammar6.irtg
+if [ -d $GDIR ]
+then
+    rm -R $GDIR
+fi
+mkdir ./$GDIR
+
+python induct/induct.py $ALIGNMENT left  nosplit   > $GDIR/grammar1.irtg
+python induct/induct.py $ALIGNMENT right nosplit   > $GDIR/grammar2.irtg
+python induct/induct.py $ALIGNMENT both  nosplit   > $GDIR/grammar3.irtg
+python induct/induct.py $ALIGNMENT left  semsplit  > $GDIR/grammar4.irtg
+python induct/induct.py $ALIGNMENT right semsplit  > $GDIR/grammar5.irtg
+python induct/induct.py $ALIGNMENT both  semsplit  > $GDIR/grammar6.irtg
 
 for i in 1 2 3 4 5 6
 do
-    scala -cp ".:bin/alto-1.1-SNAPSHOT-jar-with-dependencies.jar"  \
-          RunAll generated/grammar${i}.irtg data/string_funql.txt \
-          generated/grammar${i}_w.irtg data/string.txt generated/parsed$i.txt -Xmx4G
+    echo "-- Generate weighted grammar $i"
+    scala -J-Xmx4G -cp ".:bin/alto-1.1-SNAPSHOT-jar-with-dependencies.jar"  \
+          RunAll.scala $GDIR/grammar${i}.irtg data/string_funql.txt \
+          $GDIR/grammar${i}_em.irtg data/string.txt $GDIR/parsed$i.txt
 done
 
 for i in 1 2 3
 do
-    python induct/prepareLogLinModel.py generated/grammar${i}.irtg > generated/grammar${i}_loglin.irtg
+    echo "-- Generate grammar with $i splits"
+    python induct/prepareLogLinModel.py $GDIR/grammar${i}.irtg > $GDIR/grammar${i}_llm.irtg
 done
 
 for i in 2 3 4 5 6 7 8 9 10
 do
-    echo "Generate grammar with $i splits"
-    python induct/readIrtg.py generated/grammar3_w.irtg $i > generated/grammar3_split$i.irtg
+    echo "-- Generate grammar with $i splits"
+    python induct/readIrtg.py $GDIR/grammar3_em.irtg $i > $GDIR/grammar3_split$i.irtg
 done
 
