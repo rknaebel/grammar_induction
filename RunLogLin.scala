cbd99dfd2a5ce75086acc486e263f532148f42f6
Rene Knaebel <rknaebel@uni-potsdam.de>
2015-07-20 22:30:00 +0200

    Added RunLogLin.scala

---
+++ RunLogLin.scala
@@ -0,0 +1,73 @@
+
+import de.up.ling.irtg._
+import de.up.ling.irtg.algebra._
+import de.up.ling.irtg.automata._
+import de.up.ling.irtg.hom._
+import de.up.ling.irtg.signature._
+import de.up.ling.irtg.corpus._
+import de.up.ling.irtg.codec._
+import de.up.ling.tree._
+import de.up.ling.irtg.maxent._
+import de.saar.basic._
+import de.up.ling.irtg.util.TestingTools._
+
+
+import java.io._
+import java.util.Date
+import scala.collection.JavaConverters._
+
+import scala.language.implicitConversions;
+
+import ScalaShell._
+
+//
+// Start of the actual scala script
+//
+object RunAll extends App {
+    
+    val grammarFile = args(0)
+    val trainingCorpusFile = args(1)
+    val weightedFeatures = args(2)
+    val stringFile = args(3)
+    val parsedStringFile = args(4)
+    
+    println("Reading the grammar")
+    val irtg = loadIrtg(grammarFile).asInstanceOf[MaximumEntropyIrtg]
+    
+    println("Read the EM training corpus")
+    val corpus = irtg.readCorpus(file(trainingCorpusFile))
+
+    println("Compute parse charts and create charts.zip file")
+    Charts.computeCharts(corpus, irtg, fostream("charts.zip"))
+    corpus.attachCharts("charts.zip")
+
+    println("Start EM training")
+    irtg.trainMaxent(corpus)
+
+    new File("charts.zip").delete()
+
+    println("Write the weighted grammar")
+    val writer = new PrintWriter(new File(weightedFeatures))
+    irtg.writeWeights(writer)
+    writer.close
+
+    println("Read bulk parse corpus")
+    val testCorpus = irtg.readCorpus(file(stringFile))
+    val output = new FileWriter(parsedStringFile)
+
+    val s = "Parsed from " + testCorpus.getSource() + "\nat " + new Date().toString()
+    val cw = new CorpusWriter(irtg, true, s, output)
+    
+    println("Bulkparse corpus and print to output file")
+    irtg.bulkParse(testCorpus, cw, null)
+    
+    output.close
+}
+
+RunAll.main(args)
+
+
+
+
+
+
--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ export JAVA_HOME=/cl-tools/jdk1.8.0_05/
 export PATH=$JAVA_HOME/bin:$PATH
 export PATH=/cl-tools/scala-2.11.7/bin/:$PATH 
 
-GDIR=$1
+GDIR=./generate
+EVAL=./evaluate
 SCALA=scala
 JAVA=java
 JAVAC=javac
@@ -20,41 +21,59 @@ mkdir ./$GDIR
 
 $JAVAC -cp $ALTO -d $GDIR  extract/ConvertToLisp.java
 
-echo "-- Generate grammar 1 left  nosplit"
-$PY induct/induct.py $ALIGNMENT left  nosplit   $GDIR/grammar1.irtg $GDIR/llmtrain1.txt 
-echo "-- Generate grammar 2 right nosplit"
-$PY induct/induct.py $ALIGNMENT right nosplit   $GDIR/grammar2.irtg $GDIR/llmtrain2.txt 
-echo "-- Generate grammar 3 both  nosplit"
-$PY induct/induct.py $ALIGNMENT both  nosplit   $GDIR/grammar3.irtg $GDIR/llmtrain3.txt 
-echo "-- Generate grammar 4 left  semsplit"
-$PY induct/induct.py $ALIGNMENT left  semsplit  $GDIR/grammar4.irtg /dev/null
-echo "-- Generate grammar 5 right semsplit"
-$PY induct/induct.py $ALIGNMENT right semsplit  $GDIR/grammar5.irtg /dev/null
-echo "-- Generate grammar 6 both  semsplit"
-$PY induct/induct.py $ALIGNMENT both  semsplit  $GDIR/grammar6.irtg /dev/null
-
-for i in 1 2 3 4 5 6
+for fold in 0 1 2 3 4 5 6 7 8 9
 do
-    echo "-- Generate weighted grammar $i + bulk parsing"
-    $SCALA -J-Xmx4G -cp $ALTO RunAll.scala \
-            $GDIR/grammar${i}.irtg \
-            data/string_funql.txt \
-            $GDIR/grammar${i}_em.irtg \
-            data/string.txt \
-            $GDIR/parsed$i.txt
-    echo "-- Generate lisp format for parse $i"
-    $JAVA -cp $GDIR/:$ALTO ConvertToLisp $GDIR/grammar${i}.irtg $GDIR/parsed${i}.txt > $GDIR/parsed$i.lisp.txt
+    echo "==> Start of fold ${fold}"
+    
+    echo "-- Generate grammar 1 left  nosplit"
+    $PY induct/induct.py $ALIGNMENT left  nosplit   $GDIR/$fold/grammar1.irtg $GDIR/$fold/llmtrain1.txt 
+    echo "-- Generate grammar 2 right nosplit"
+    $PY induct/induct.py $ALIGNMENT right nosplit   $GDIR/$fold/grammar2.irtg $GDIR/$fold/llmtrain2.txt 
+    echo "-- Generate grammar 3 both  nosplit"
+    $PY induct/induct.py $ALIGNMENT both  nosplit   $GDIR/$fold/grammar3.irtg $GDIR/$fold/llmtrain3.txt 
+    echo "-- Generate grammar 4 left  semsplit"
+    $PY induct/induct.py $ALIGNMENT left  semsplit  $GDIR/$fold/grammar4.irtg /dev/null
+    echo "-- Generate grammar 5 right semsplit"
+    $PY induct/induct.py $ALIGNMENT right semsplit  $GDIR/$fold/grammar5.irtg /dev/null
+    echo "-- Generate grammar 6 both  semsplit"
+    $PY induct/induct.py $ALIGNMENT both  semsplit  $GDIR/$fold/grammar6.irtg /dev/null
+    
+    for i in 1 2 3 4 5 6
+    do
+        echo "-- Generate weighted grammar $i + bulk parsing"
+        $SCALA -J-Xmx4G -cp $ALTO RunAll.scala \
+                $GDIR/$fold/grammar${i}.irtg \
+                data/string_funql.txt \
+                $GDIR/$fold/grammar${i}_em.irtg \
+                data/string.txt \
+                $GDIR/$fold/parsed$i.txt
+        echo "-- Generate lisp format for parse $i"
+        $JAVA -cp $GDIR:$ALTO ConvertToLisp $GDIR/$fold/grammar${i}.irtg $GDIR/$fold/parsed${i}.txt > $GDIR/$fold/parsed$i.lisp.txt
+    done
+
+    for i in 1 2 3
+    do
+        echo "-- Generate loglinear grammar $i"
+        $PY induct/prepareLogLinModel.py $GDIR/$fold/grammar${i}.irtg > $GDIR/$fold/grammar${i}_llm.irtg
+        echo "-- Generate features weights for grammar $i + bulk parsing"
+        $SCALA -J-Xmx4G -cp $ALTO RunLogLin.scala \
+                $GDIR/$fold/grammar${i}_llm.irtg \
+                data/string_funql.txt \
+                $GDIR/$fold/grammar${i}_features.irtg \
+                data/string.txt \
+                $GDIR/$fold/parsed$i_llm.txt
+
+        echo "-- Generate lisp format for llm parse $i"
+        $JAVA -cp $GDIR/:$ALTO ConvertToLisp $GDIR/$fold/grammar${i}.irtg $GDIR/$fold/parsed${i}.txt > $GDIR/$fold/parsed$i_llm.lisp.txt
+
+    done
+
+    for i in 2 3 4 5 6 7 8 9 10
+    do
+        echo "-- Generate grammar with $i splits"
+        $PY induct/splitGrammar.py $GDIR/$fold/grammar3_em.irtg $i > $GDIR/$fold/grammar3_split$i.irtg
+    done
+    
 done
 
-for i in 1 2 3
-do
-    echo "-- Generate loglinear grammar $i"
-    $PY induct/prepareLogLinModel.py $GDIR/grammar${i}.irtg > $GDIR/grammar${i}_llm.irtg
-done
-
-for i in 2 3 4 5 6 7 8 9 10
-do
-    echo "-- Generate grammar with $i splits"
-    $PY induct/splitGrammar.py $GDIR/grammar3_em.irtg $i > $GDIR/grammar3_split$i.irtg
-done
 
